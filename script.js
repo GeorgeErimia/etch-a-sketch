8f8d5084a93c064444025e06275577fe1da04844
George Erimia <l3ttuceftw@gmail.com>
2024-05-17 20:22:44 +0300

    Add 'mouseover' event listener to squares | Configure listener to change div's color

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 const container = document.querySelector('.container');
+const gridSize = 16;
 
 // Create a 16x16 grid of square divs
-for(let i = 0; i < 16; i++) {
+for(let i = 0; i < gridSize * gridSize; i++) {
     const div = document.createElement('div')
     div.classList.add('square');
     div.classList.add(`square${i}`);
-    div.textContent = i;
+    div.style.width = `${100 * (1/gridSize)}%`;
+    div.style.height= `${100 * (1/gridSize)}%`;
+    // div.textContent = i;
     container.appendChild(div);
 
     // console.log("WIDTH: " + window.innerWidth)
     // console.log("HEIGHT: " + window.innerHeight)
+
+    // Set the 'hover' effect of the div to change color whenever mouse passes over it 
+    div.addEventListener('mouseover', () => {
+        div.style.backgroundColor = "black";
+    })
 }
+
