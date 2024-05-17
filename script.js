71d3c273abcf942f7a79a0bd27c53eb5c00bf722
George Erimia <l3ttuceftw@gmail.com>
2024-05-17 23:00:35 +0300

    Add 'Change Grid Size' button

--- script.js
+++ script.js
@@ -1,22 +1,51 @@
 const container = document.querySelector('.container');
-const gridSize = 16;
+const button = document.querySelector('.btn-resize');
+const body = document.querySelector('body');
 
-// Create a 16x16 grid of square divs
-for(let i = 0; i < gridSize * gridSize; i++) {
-    const div = document.createElement('div')
-    div.classList.add('square');
-    div.classList.add(`square${i}`);
-    div.style.width = `${100 * (1/gridSize)}%`;
-    div.style.height= `${100 * (1/gridSize)}%`;
-    // div.textContent = i;
-    container.appendChild(div);
+function createGame(gridSize) {
+    // reset grid container
+    container.innerHTML = "";
 
-    // console.log("WIDTH: " + window.innerWidth)
-    // console.log("HEIGHT: " + window.innerHeight)
-
-    // Set the 'hover' effect of the div to change color whenever mouse passes over it 
-    div.addEventListener('mouseover', () => {
-        div.style.backgroundColor = "black";
-    })
+    // Create a gridSize x gridSize of square divs
+    for(let i = 0; i < gridSize * gridSize; i++) {
+        const div = document.createElement('div')
+        div.classList.add('square');
+        div.classList.add(`square${i}`);
+        div.style.width = `${100 * (1/gridSize)}%`;
+        div.style.height= `${100 * (1/gridSize)}%`;
+        div.style.opacity = 0.0;
+    
+        // Set the 'hover' effect of the div to change color whenever mouse passes over it 
+        div.addEventListener('mouseover', () => {
+            if(parseInt(div.style.opacity) < 1) {
+                div.style.opacity = parseFloat(div.style.opacity) + 0.1;
+            }
+        })
+    
+        // div.textContent = i;
+        container.appendChild(div);
+    
+        // console.log("WIDTH: " + window.innerWidth)
+        // console.log("HEIGHT: " + window.innerHeight)
+    }
 }
+
+createGame(16);
+
+function changeGridSize() {
+    const newGridSize = prompt('Insert new grid size');
+    const newGridSizeValue = parseInt(newGridSize);
+    if(newGridSizeValue > 100) {
+        alert('A maximum value of 100 is required')
+    } else {
+        createGame(newGridSizeValue)
+    }
+}
+
+button.addEventListener('click', changeGridSize)
+
+
+
+
+
 
